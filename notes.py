307928aa04c710aa2d3d1384d0cc8680b54a2ebe
sneha316200 <musalaesneha@gmail.com>
2024-09-04 14:53:28 +0000

    Finished taking notes, going to start program 2

---
+++ notes.py
@@ -0,0 +1,14 @@
+name = input("Please tell me your name!")
+print("Hi Tim")
+print (name+ "is a pretty cool name")
+print("I wish my name was" +name)
+
+
+
+
+
+name= input("Tell me your name now!")
+email= input("Ler me send you spam")
+nickanme= input("What do your friends call you?")
+
+print("Hey" + name "or,should I say" + nickanme + ".")
